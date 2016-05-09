c30b7ec2109e0946ef3789fca054590e026db5aa
Juan Miret <juanmiret55@icloud.com>
2016-05-09 01:30:51 -0300

    Removed secret keys

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ set :deploy_to, '/home/deploy/universe'
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
 
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
